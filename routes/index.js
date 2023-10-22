df3fdf78ed036cfc08b4ad294613728830576680
Krimothiazine <krimouv1995@gmail.com>
2023-10-22 19:39:53 +0100

    Create and style app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini MessageBoard', messages: messages });
+});
+
+// new message page
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Send Message' });
+});
+
+router.post('/new', function (req, res, next) {
+  const userName = req.body.userName;
+  const userMessage = req.body.userMessage;
+  const added = new Date();
+  messages.push({ user: userName, text: userMessage, added });
+  res.redirect('/');
 });
 
 module.exports = router;
