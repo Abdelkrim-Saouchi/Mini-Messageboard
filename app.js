7f231ea5dbbeb941cc8ede1c9bcaf2fcdc139b7b
Krimothiazine <krimouv1995@gmail.com>
2023-10-29 09:31:30 +0100

    Solving buffering timeout

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const { error } = require('console');
 
 var app = express();
 
+// connect to mongoDb
+main().catch((error) => console.log(error));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,12 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// connect to mongoDb
-main().catch((error) => console.log(error));
-
-async function main() {
-  await mongoose.connect(process.env.MONGODB_URL);
-}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
